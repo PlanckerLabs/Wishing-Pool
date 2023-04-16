da280fa50a1e476b5135d6d85f4e3ed9134c0ff4
0xRory <0x1rory@gmail.com>
2023-04-16 17:53:30 +0800

    flex

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -97,20 +97,121 @@ const Hero = () => {
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
                   Why We Need the Wishing Pool?
                 </h1>
-                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                   In real life, a Wishing Pool is a place where people reflect on and express their inner hopes for the future. It transcends cultural and geographical barriers and exists widely in every corner of society where people have hopes for better social realities. Through ritualistic gestures, people entrust their wishes to gods or the universe. Alternatively, by sharing their wishes with those around them through sincere and imaginative public expressions in this symbolic public space, people create connections between individuals, and give blessings to each other.
                 </p>
-                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                   Every wish, whether private or public, is a call from the individual to the desired reality in their heart. The Wishing Pool is where these diverse social calls converge. Based on physical space, the Wishing Pool can only convey these wishes to a limited extent. The process of making wishes come true depends more on the individual's efforts, or the favor of the gods.
                 </p>
-                <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                   In today's era of information technology, if we could link each other's wishes through the internet, and let these imaginative wishes for society be seen by each other, perhaps we could connect more people with similar expectations, and make some consensus wishes come true.
                 </p>
-                <p>
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
                   Building a globally shared Wishing Pool based on blockchain infrastructure is to make the social calls of this era visible to the public and to truly trigger them to become a certain reality.
                 </p>
                 <p className="pb-5">
                   Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world. ——Albert Einstein
+                </p>
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  Making Wishes, as a Form of Social Imagination
+                </h1>
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+
+
+                  When we say "we want to build a better world", what specific realities does this wish refer to?
+
+
+
+                  Individuals from different cultural backgrounds will have radically different concrete expressions when imagining a better society. These diverse expressions are not created out of thin air; rather, they stem from a profound understanding and experience of the current situation, and extract expectations for this reality from the sensory experience of real life.
+
+
+
+                  Therefore, there is no unified standard for a "better world". Talking about changing the world without starting from reality is empty, and technical production that is detached from real needs is redundant and misplaced.
+
+
+
+                  Social imagination is not boundless and fanciful, but a creative way of thinking that is closely tied to reality yet not limited by immediate obstacles. And making a wish, is an open expression of this creative thinking.
+
+
+
+                  In order to achieve the wish of building a better world, we need individuals from different backgrounds to voice their own imaginations in response to their environments. Each vision of a better society is unique, and no concrete wish raised from a specific reality can be expressed by others.
+
+
+
+                  The Wishing Pool is a globally shared public domain that bears people's different imaginations of a better world, and serves as a platform for public expressions.
+
+                  ##
+                </p>
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  Starting from Individuals and Communities
+                </h1>
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+
+
+                  We live in an era where the world order is plunging into deeper chaos. International conflicts and wars occur frequently, the global political environment is constantly worsening, economic uncertainty brings more friction to international trade. The globalization that was supposed to be a human ideal for collaboration has shown such a fragile and broken reality today. Faced with these great conflicts, no solution can permanently quell our anxiety.
+
+
+                  However, as independent individuals who love this world, even if we cannot shake those deep-rooted diseases on our own, we still have the ability to imagine a better society. This valuable ability to imagine can guide us to find like-minded people and work together to build a small environment that allows us to live comfortably.
+
+
+
+                  **A better society should be composed of individuals and their communities as the smallest unit.** Whether it is the emergence of Bitcoin or the birth of the Network State narrative, it all originates from a call made by individuals in response to the reality. These social callings continuously attract more like-minded people to join, slowly forming its own communities. Over a long period of growth, they will eventually converge into a force that change the world.
+
+
+
+                  Therefore, DO NOT succumb to a world that you don't agree with.
+
+
+
+                  Do not give up your innate desire for a better life.
+
+
+
+                  **Perceive your deepest desires, mobilize your most sincere imagination, and publicly articulate the reality you expect from this world. Make a wish.**
+
+
+
+                  This wish can be very ambitious, even if as an individual you may not be able to achieve it. However, The Wishing Pool shared globally would allow people who support this wish to find each other and work together to negotiate specific ways to achieve it.
+
+
+
+                  This wish can be very small, even if it may only be related to improving the community environment you are in. The Wishing Pool based on transparent on-chain infrastructure may help you gain support from nearby residents, making it more likely for your wish to come true.
+
+
+
+                  **The Wishing Pool is a cyber public square for everyone to make wishes and receive their blessings and echos. It is a trigger to the social callings.**
+                </p>
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  The Rise of Regional Public Goods
+                </h1>
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+                  When people start forming diverse communities based on different consensuses, some wishes proposed by the community as a whole will begin to emerge.
+                  A wish proposed by the community has gone through an internal negotiation process and reached a certain consensus foundation. However, it may not necessarily be achievable through the coordination and allocation of community resources alone. Publicly expressing these wishes means that the community is requesting support from the external environment.
+                  However, in reality, boundaries often exist between different communities due to differences in values. These boundaries can lead to a lack of communication and exchange channels, which hinders cross-community collaboration.
+                  The Wishing Pool is a platform for everyone to openly express their wishes. It can bridge value differences between communities by focusing on understanding each other's expectations, finding common ground, and launching cross-community cooperation. This collaboration can refine and negotiate common needs of local communities, form collective solutions, gather more people's support, generate regional public goods, and promote larger-scale social changes.
+                  **The Wishing Pool serves as platform that provides public space for diverse communities. It promotes the integration of various social imagination, breaks the boundaries between communities with common wishes, and will become a social support system that promotes regional resource coordination and distribution.**
+                </p>
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  The Whole Earth Wish Catalog
+                </h1>
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+                  In 1968, Stewart Brand brought a global perspective to the world through "The Whole Earth Catalog". At a time when the internet did not yet exist, he inspired self-education and the creation of personally-approved environments by passing on information about available tools.
+                  Today, with internet technology being widely available, access to tools and information is no longer a barrier for people to build a better world. However, people still lack a global perspective when it comes to their expectations for a better world. This global perspective does not imply a uniform worldview, but rather a dynamic map of expectations with significant regional differences due to cultural diversity.
+                  The Wishing Pool is a globally shared platform for making wishes. It provides people with an overview of wishes from around the world, which not only deepens their understanding of social expectations worldwide, but also promotes cross-regional cooperation. This platform enables the realization of diverse wishes to create a better society.
+                  This “Whole Earth Wish Catalog” also provides a map of the world for nomads who do not want to be constrained by factors like nationality, race, or geography. We can flow around the world to places where the wishes we want to achieve are happening and build a world with more choices together.
+                  **The Wishing Pool serves as a starting point for us to share our imagination of society through wishing and to work together towards a better future.**
+                </p>
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  Worldwide Network to Support Your Wishes
+                </h1>
+                <p className="mb-12 text-base text-justify font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
+
+                  We believe that the new world of the future is driven by good wishes.
+                  We believe that the public expression of good wishes is a social calling.
+                  We believe that this calling can transcend language, geography, culture, and faith barriers, and become a global network that supports the fulfillment of wishes.
+                  We look forward to this network becoming a public domain co-created by humanity, where everyone can unleash their creativity and imagination, and become a new social force that injects infinite possibilities into the future of humanity.
+                  **The Wishing Pool is a public good for everyone.**
+                  **Let us light up the future together with our wishes.**
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
 
@@ -191,7 +292,7 @@ const Hero = () => {
                     Signature
                   </button>
                 </div>
-                <h1 className="pr-5">
+                <h1 className="pt-6">
                   {
                     (isConnected && powers.length > 0) ? <p>Wish Power:{power}</p> : 'Wish Power: 0'
                   }
